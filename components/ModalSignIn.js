2db6126f185609ea0b1f79e1dfb02c0df933790c
maumausaurus <maureen.duplex@edhec.com>
2021-03-14 20:13:11 +0100

    bye useless close buttons

--- components/ModalSignIn.js
+++ components/ModalSignIn.js
@@ -22,12 +22,6 @@ const ModalSignIn = () => {
               closeModal={onClose}
             />
           </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="purple" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
--- components/ModalSignUp.js
+++ components/ModalSignUp.js
@@ -27,12 +27,6 @@ const ModalSignUp = () => {
               closeModal={onClose}
             />
           </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
