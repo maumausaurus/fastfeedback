61e8df70fafb98190ccca1010c4e880ebfc8a84f
maumausaurus <maureen.duplex@edhec.com>
2021-03-16 18:40:11 +0100

    la modal add site est connectée à firestore !!

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -1,20 +1,29 @@
 import { Button, Container, FormControl, FormLabel, Input, FormErrorMessage, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
 import { useForm } from "react-hook-form";
-import { useState } from 'react';
+// import { useState } from 'react';
+import firebase from '../lib/firebase';
+
+const firestore = firebase.firestore()
+console.log(firestore)
 
 const AddSiteModal = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { register, handleSubmit, watch, errors } = useForm();
-    
-    const onSubmit = (data) => {
+
+    const onSubmit = async (data) => {
         //create site 
         try {
-            console.log("data :",data)
-            // créer une entrée dans la base de données en passant les valeurs de siteName et siteLink
-            // createSite(siteName,siteLink);
+            console.log("data :", data)
             
-        } catch (e) {
-        // jsp quoi mettre ?
+            // créer une entrée dans la base de données en passant les valeurs de siteName et siteLink
+            const res = await firestore.collection('sites').add({
+                link: data.siteLink,
+                name: data.siteName
+            });
+            // createSite(siteName,siteLink);
+
+        } catch (error) {
+            console.log(error)
         }
     }
 
@@ -29,7 +38,7 @@ const AddSiteModal = () => {
 
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
-                <ModalContent as ="form"
+                <ModalContent as="form"
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <ModalHeader>Add site</ModalHeader>
@@ -38,31 +47,31 @@ const AddSiteModal = () => {
                         <Container display="flex" flexDirection="column" alignItems="center">
                             <FormControl>
                                 <FormLabel>Name</FormLabel>
-                                <Input 
-                                    placeholder="Name of your site" 
+                                <Input
+                                    placeholder="Name of your site"
                                     name="siteName"
-                                    ref={register ({ required : true })}
-                                    />
+                                    ref={register({ required: true })}
+                                />
                                 <FormErrorMessage>Error message</FormErrorMessage>
                             </FormControl>
                             <FormControl>
                                 <FormLabel>Site url</FormLabel>
-                                <Input 
-                                    placeholder="https://greatstuff.com" 
+                                <Input
+                                    placeholder="https://greatstuff.com"
                                     name="siteLink"
-                                    ref={register ({ required : true })}
-                                    />
+                                    ref={register({ required: true })}
+                                />
                                 <FormErrorMessage>Error message</FormErrorMessage>
                             </FormControl>
                         </Container>
                     </ModalBody>
                     <ModalFooter>
-                        <Button 
+                        <Button
                             type="submit"
-                            variant="solid" 
-                            size="md" 
+                            variant="solid"
+                            size="md"
                             colorScheme="purple"
-                            >
+                        >
                             Create
                             </Button>
                     </ModalFooter>
